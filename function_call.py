11a73d58f50cdcfd45696f6106525570afc3d9c6
Rohit Verma <94777918+roohit-rv@users.noreply.github.com>
2023-05-16 02:50:09 +0530

    Add files via upload

---
+++ function_call.py
@@ -0,0 +1,6 @@
+def greet(name):
+    print("hi ",name)
+    print("how do you do?", name)
+
+name=input("What is your Name? ")
+greet(name)
---
+++ round_up.py
@@ -0,0 +1,5 @@
+# import math then use the function math.ceil to round decimal number to whole number
+import math
+n1=int(input("enter the first number"))
+n2=int(input("enter the second number"))
+print(math.ceil(n1/n2))
